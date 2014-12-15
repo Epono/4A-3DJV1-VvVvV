570840b957e8de56119b9b1a446937d4f8c40481
LeRyokan <tixier.lucas@gmail.com>
2014-12-15 22:19:03 +0100

    Update GameScene for TrapRoom

--- Assets/Scripts/Game/IsKinematic.cs
+++ Assets/Scripts/Game/IsKinematic.cs
@@ -1,42 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IsKinematic : MonoBehaviour
-{
+public class IsKinematic : MonoBehaviour {
+
     [SerializeField]
     private Rigidbody TargetRigibody;
 
-    void Start()
-    {
+//  [SerializeField]
+//  private Vector3 _force;
+    
 
+    // Use this for initialization
+    void Start () {
+    
     }
-
+    
     // Update is called once per frame
-    void Update()
-    {
-        /*
-        if (Input.GetKey(KeyCode.A))
-        {
-            TargetRigibody.isKinematic = false;
-            TargetRigibody.AddForce(Vector3.down);
-        }
-        */
+    void Update () {
+    
+    //  if (Input.GetKey (KeyCode.A)) {
+    //                  TargetRigibody.isKinematic = false;
+    //      TargetRigibody.AddForce(Vector3.down);
+    //          }
+
+
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        //if (other is Player)
-        // {
-        Debug.Log("Intruder !!!!");
+    void OnTriggerEnter(Collider other) {
+        //other.rigidbody.isKinematic = true;
+    
+    //  other.rigidbody.AddForce(_force);
+        Debug.Log("Un intru !");
         TargetRigibody.isKinematic = false;
-        TargetRigibody.useGravity = true;
         TargetRigibody.AddForce(Vector3.down);
-        // }
+
     }
 
     public void OnDrawGizmos()
     {
         //Debug.DrawLine(this.transform.position, this.transform.position + _force / Physics.gravity.magnitude, Color.red);
-        Debug.DrawLine(this.transform.position, TargetRigibody.transform.position, Color.yellow);
+        Debug.DrawLine (this.transform.position, TargetRigibody.transform.position, Color.yellow);
     }
+
 }
