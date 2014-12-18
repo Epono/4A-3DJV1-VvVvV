043cc822889e1b6dd7583d8b92e25ae3d2adc01d
LeRyokan <tixier.lucas@gmail.com>
2014-12-18 18:04:56 +0100

    Fix errors

--- Assets/Scripts/Game/PlayerScript.cs
+++ Assets/Scripts/Game/PlayerScript.cs
@@ -32,9 +32,26 @@ public class PlayerScript
     {
 
     }
-
+/*
     public void ExecuteAction(string actionName)
     {
 
+    }
+ */
+
+    public void ExecuteActionT(CharacterAction action)
+    {
+        /*
+        if (this.transform.position != action.getLocation())
+        {
+            Debug.Log(gameObject.name + "Execute action :" + action.GetActionName());
+            _agent.SetDestination(action.getLocation());
+            Debug.Log("Mon_AgentPATH:" + _agent.path);
+
+            Debug.Log("JE VAIS A LA POSITION :" + action.getLocation());
+        }
+        //_agent.ResetPath();
+        // throw new System.NotImplementedException();
+       */
     }
 }
